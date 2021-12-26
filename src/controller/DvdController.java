8ca42255a04c16cd1fe0de0d3c85f58acae7c280
NoNutNghia <nghia.nn260701@gmail.com>
2021-12-27 01:11:05 +0700

    Code Controller cho DVD

---
+++ src/controller/DvdController.java
@@ -0,0 +1,33 @@
+package controller;
+
+import models.Dvd;
+import service.DvdService;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class DvdController {
+    private final DvdService dvdService;
+
+    public DvdController(DvdService dvdService) {
+        this.dvdService = dvdService;
+    }
+
+    //Lay du lieu tu db
+
+    public List<Dvd> findAll() throws SQLException {
+        return dvdService.findAll();
+    }
+
+    public void insert(Dvd dvd) {
+        dvdService.insertDvd(dvd);
+    }
+
+    public void update(Dvd dvd) {
+        dvdService.updateDvd(dvd);
+    }
+
+    public void delete(int id) {
+        dvdService.deleteDvd(id);
+    }
+}
